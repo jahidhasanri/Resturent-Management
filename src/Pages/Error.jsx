ed5e323bf901235aeb1a5a36cbe7cb5e0f3bfc3b
jahid hasan rifat <jahadhasanrifat@gmail.com>
2025-02-12 16:37:31 +0600

    update error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
        <div>
-        <Navbar></Navbar>
+        {/* <Navbar></Navbar> */}
+        <Link to='/' className='btn m-2'>Back to Home</Link>
          <div className='text-center mt-[300px]'>
         <div className='w-full md:w-[500px] lg:h-[500px] mx-auto '>
             <img className='' src="https://i.ibb.co.com/PQXhmKJ/404-page-not-found-1024x576.webp" alt="" />
         </div>
         </div>
-        <Footer></Footer>
+        {/* <Footer></Footer> */}
        </div>
     );
 };
